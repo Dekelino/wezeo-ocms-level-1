ce96797178be556e695acd93a6c349cae8684b50
Filip Oravec <106304843+Dekelino@users.noreply.github.com>
2023-12-15 15:06:19 +0100

    Status 15.12
    Ive created ocms project using artisan console, last task done: create controller (Arrivals)

---
+++ plugins/app/arrival/Plugin.php
@@ -0,0 +1,95 @@
+<?php namespace App\Arrival;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Arrival Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Arrival',
+            'description' => 'No description provided yet...',
+            'author'      => 'App',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+
+        return [
+            'App\Arrival\Controllers\Arrivals' => 'arrivals',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'app.arrival.some_permission' => [
+                'tab' => 'Arrival',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'arrival' => [
+                'label'       => 'Arrival',
+                'url'         => Backend::url('app/arrival/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['app.arrival.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
---
+++ plugins/app/arrival/updates/create_arrivals_table.php
@@ -0,0 +1,24 @@
+<?php namespace App\Arrival\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+/**
+ * CreateArrivalsTable Migration
+ */
+class CreateArrivalsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('app_arrival_arrivals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('app_arrival_arrivals');
+    }
+}
